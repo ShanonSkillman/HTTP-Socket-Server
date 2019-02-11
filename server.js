07e9e04ef15ecba011c5d228252ba288cc348d80
Djavan Cackley <djavancackley@Djavans-MacBook-Pro.local>
2019-02-10 22:42:50 -1000

    grabbing bits of HTML header

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 const net = require('net');
+const tempPage = require("./tempPage.js")
+// const errorDoc = require("./404.js")
+// const heliumDoc = require("./helium.js")
+// const hydroDoc = require("./hydrogren.js")
+// const indexhtmlDoc = require("./styles.css.js")
 
-const server = net.createServer((c) => {
+const clientConnections = [];
+
+const server = net.createServer((socket) => {
     console.log('client connected');
-    c.on('end', () => {
-        console.log('client disconnected');
+    socket.on('data', data => {
+        console.log(data)
+        let parsedData = data.toString()
+        console.log(parsedData)
+        parsedData = parsedData.split('\n')
+        console.log(parsedData)
+        let requestLine = parsedData[0].split(' ')
+        console.log(requestLine)
+        let method = requestLine[0]
+        console.log(method)
+        let uriLine = requestLine[2]
+        console.log(uriLine)
+
+        console.log(Date())
+
+
+
+        socket.write(tempPage.clientMsg);
+        socket.end();
     });
-    c.write('we fuckin made it\r\n');
-    c.pipe(c);
 });
+
+
 server.on('error', (err) => {
     throw err;
 });
 server.listen(8080, () => {
     console.log('server bound')
 });
+
---
+++ tempPage.js
@@ -0,0 +1,5 @@
+const clientMsg = 'HTTP/1.1 \n\n <h1>FUKIN BEAUTFIUL</h1>'
+
+module.exports = {
+    clientMsg: clientMsg
+}
