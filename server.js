fef9e722572ff513b1d3fb9567cfa26db3ee8721
Djavan Cackley <djavancackley@Djavans-MacBook-Pro.local>
2019-02-11 18:39:53 -1000

    going backwards to debug to  move forwards

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const net = require('net');
 const tempPage = require("./tempPage.js")
 // const errorDoc = require("./404.js")
-const heliumDoc = require("./helium.js")
+// const heliumDoc = require("./helium.js")
 // const hydroDoc = require("./hydrogren.js")
 // const indexhtmlDoc = require("./styles.css.js")
 
@@ -12,35 +12,32 @@ const server = net.createServer((socket) => {
     socket.on('data', data => {
         console.log(data)
         let parsedData = data.toString()
-        console.log(parsedData)
+        console.log('parsedData to string', parsedData)
         parsedData = parsedData.split('\n')
-        console.log(parsedData)
+        console.log('parsedData', parsedData)
         let requestLine = parsedData[0].split(' ')
-        console.log(requestLine)
+        console.log('requestLine', requestLine)
         let method = requestLine[0]
-        console.log(method)
+        console.log('method', method)
         let requestURL = requestLine[1]
-        console.log(requestURL)
+        console.log('requestURL', requestURL)
 
         console.log(Date() + ` ${method} ` + `${requestURL}`);
 
-        if (method === 'GET') {
-            if (requestURL === '/') {
-                socket.write(
-                    `HTTP/1.1 200 OK\nServer: nginx/1.4.6 (Unbuntu)\nContent-Type: text/html; charset=utf-8\n\n${}`);
-                socket.end();
-            } else if (requestURL === '/.html') {
-                socket.write()
-            }
-        }
-    }
-
-
-
-        socket.write(tempPage.clientMsg);
-    socket.end();
-});
-});
+        // if (method === 'GET') {
+        //     if (requestURL === '/') {
+        //         socket.write(
+        //             `HTTP/1.1 200 OK\nServer: nginx/1.4.6 (Unbuntu)\nContent-Type: text/html; charset=utf-8\n\n${}`);
+        //         socket.end();
+        //     } else if (requestURL === '/.html') {
+        //         socket.write()
+        //     }
+        // }
+        // socket.write(tempPage.clientMsg);
+        // socket.end();
+    });
+    // });
+})
 
 
 server.on('error', (err) => {
@@ -49,4 +46,3 @@ server.on('error', (err) => {
 server.listen(8080, () => {
     console.log('server bound')
 });
-
